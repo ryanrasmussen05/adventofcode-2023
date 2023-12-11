efb9441043490cd117909dbf88d15fad33de624c
Ryan Rasmussen <rrasmussen@linkedin.com>
2023-12-11 12:25:03 -0600

    catch up

--- day-10/solution.js
+++ day-10/solution.js
@@ -1,3 +1,85 @@
 import { getInput } from '../util/file-reader.js';
 
 const input = await getInput('./day-10/input.txt');
+
+// part 1
+const start = findStart();
+const visitedPipes = [start];
+while (visitNextPipe(visitedPipes)) {
+  continue;
+}
+console.log(visitedPipes.length / 2);
+
+// part 2
+
+
+function findStart() {
+  const sLine = input.find(line => line.includes('S'));
+  const sRow = sLine.indexOf('S');
+  const row = input.indexOf(sLine);
+  const column = sRow;
+
+  const topNode = input[row - 1]?.[column] || '.';
+  const bottomNode = input[row + 1]?.[column] || '.';
+  const leftNode = input[row][column - 1] || '.';
+  const rightNode = input[row][column + 1] || '.';
+
+  const doesTopConnect = topNode === '|' || topNode === '7' || topNode === 'F';
+  const doesBottomConnect = bottomNode === '|' || bottomNode === 'J' || bottomNode === 'L';
+  const doesLeftConnect = leftNode === '-' || leftNode === 'L' || leftNode === 'F';
+  const doesRightConnect = rightNode === '-' || rightNode === 'J' || rightNode === '7';
+
+  let startCharacter = '';
+  if (doesTopConnect && doesBottomConnect) startCharacter = '|';
+  if (doesLeftConnect && doesRightConnect) startCharacter = '-';
+  if (doesTopConnect && doesRightConnect) startCharacter = 'L';
+  if (doesTopConnect && doesLeftConnect) startCharacter = 'J';
+  if (doesBottomConnect && doesLeftConnect) startCharacter = '7';
+  if (doesBottomConnect && doesRightConnect) startCharacter = 'F';
+
+  return { row, column, character: startCharacter };
+}
+
+function visitNextPipe(visitedPipes) {
+  const currentPipe = visitedPipes[visitedPipes.length - 1];
+  let pipesToVisit = [];
+  if (currentPipe.character === '|') {
+    pipesToVisit.push({ row: currentPipe.row - 1, column: currentPipe.column });
+    pipesToVisit.push({ row: currentPipe.row + 1, column: currentPipe.column });
+  }
+  if (currentPipe.character === '-') {
+    pipesToVisit.push({ row: currentPipe.row, column: currentPipe.column - 1 });
+    pipesToVisit.push({ row: currentPipe.row, column: currentPipe.column + 1 });
+  }
+  if (currentPipe.character === 'L') {
+    pipesToVisit.push({ row: currentPipe.row - 1, column: currentPipe.column });
+    pipesToVisit.push({ row: currentPipe.row, column: currentPipe.column + 1 });
+  }
+  if (currentPipe.character === 'J') {
+    pipesToVisit.push({ row: currentPipe.row - 1, column: currentPipe.column });
+    pipesToVisit.push({ row: currentPipe.row, column: currentPipe.column - 1 });
+  }
+  if (currentPipe.character === '7') {
+    pipesToVisit.push({ row: currentPipe.row + 1, column: currentPipe.column });
+    pipesToVisit.push({ row: currentPipe.row, column: currentPipe.column - 1 });
+  }
+  if (currentPipe.character === 'F') {
+    pipesToVisit.push({ row: currentPipe.row + 1, column: currentPipe.column });
+    pipesToVisit.push({ row: currentPipe.row, column: currentPipe.column + 1 });
+  }
+
+  const firstPipeToTry = pipesToVisit[0];
+  const isFirstPipeVisited = visitedPipes.some(pipe => pipe.row === firstPipeToTry.row && pipe.column === firstPipeToTry.column);
+  const secondPipeToTry = pipesToVisit[1];
+  const isSecondPipeVisited = visitedPipes.some(pipe => pipe.row === secondPipeToTry.row && pipe.column === secondPipeToTry.column);
+
+  if (!isFirstPipeVisited) {
+    visitedPipes.push({ row: firstPipeToTry.row, column: firstPipeToTry.column, character: input[firstPipeToTry.row][firstPipeToTry.column] });
+    return true;
+  } else if (!isSecondPipeVisited) {
+    visitedPipes.push({ row: secondPipeToTry.row, column: secondPipeToTry.column, character: input[secondPipeToTry.row][secondPipeToTry.column] });
+    return true;
+  } else {
+    return false;
+  }
+}
--- day-11/solution.js
+++ day-11/solution.js
@@ -1,3 +1,75 @@
 import { getInput } from '../util/file-reader.js';
 
 const input = await getInput('./day-11/input.txt');
+
+// part 1
+let starMap = [...input];
+expandRowsWithoutGalaxies();
+expandColumnsWithoutGalaxies();
+const galaxies = getGalaxyObjects();
+console.log(sumDistanceBetweenGalaxies());
+
+
+function expandRowsWithoutGalaxies() {
+  let updatedStarMap = [];
+
+  starMap.forEach((row, rowIndex) => {
+    updatedStarMap.push(row);
+    if (!row.includes('#')) {
+      updatedStarMap.push(row);
+    }
+  });
+
+  starMap = updatedStarMap;
+}
+
+function expandColumnsWithoutGalaxies() {
+  let columnsWithoutGalaxies = [];
+  let updatedStarMap = [...starMap];
+
+  for (let i = 0; i < starMap[0].length; i++) {
+    if (starMap.every(row => row[i] !== '#')) {
+      columnsWithoutGalaxies.push(i);
+    }
+  }
+
+  columnsWithoutGalaxies.forEach((column, index) => {
+    const tempStarMap = [];
+    updatedStarMap.forEach(row => {
+      tempStarMap.push(row.slice(0, column + 1 + index) + '.' + row.slice(column + 1 + index));
+    });
+    updatedStarMap = tempStarMap;
+  });
+
+  starMap = updatedStarMap;
+}
+
+function getGalaxyObjects() {
+  let currGalaxy = 1;
+  const galaxyObjects = [];
+
+  starMap.forEach((row, rowIndex) => {
+    row.split('').forEach((character, columnIndex) => {
+      if (character === '#') {
+        galaxyObjects.push({ column: columnIndex, row: rowIndex, number: currGalaxy });
+        currGalaxy++;
+      }
+    });
+  });
+
+  return galaxyObjects;
+}
+
+function getDistanceBetweenGalaxies(galaxy1, galaxy2) {
+  return Math.abs(galaxy1.column - galaxy2.column) + Math.abs(galaxy1.row - galaxy2.row);
+}
+
+function sumDistanceBetweenGalaxies() {
+  let sum = 0;
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      sum += getDistanceBetweenGalaxies(galaxies[i], galaxies[j]);
+    }
+  }
+  return sum;
+}
